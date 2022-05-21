83af54e41344e5e4a70345e44f47288920ab3f7e
saimanoharg <iammanohar1997@gmail.com>
2022-05-21 14:57:02 +0530

    Commiting Arthemic file

---
+++ Arthmetic.sh
@@ -0,0 +1,11 @@
+name=SaiManohar
+age=24
+
+echo "my name is $name and my age is $age"
+echo "i have lived 10  years in my home town and 14 years in hostel, total" `expr 10 + 14`
+
+echo "Here is the question"
+echo 'If you are having 3 baskets full of 15 apples each, whats the total apples you have?'
+echo "Answer is"
+
+expr 3 \* 15
